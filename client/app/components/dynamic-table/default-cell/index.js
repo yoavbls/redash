cca3a3ead2447c9d314234fb1579ba0ed0bc7377
Yoav <yoavbls@gmail.com>
2019-04-13 14:29:56 +0300

    Add simple elements that renders with react2angular

--- client/app/components/dynamic-table/default-cell/index.js
+++ client/app/components/dynamic-table/default-cell/index.js
@@ -1,5 +1,9 @@
 import { includes, identity } from 'lodash';
 import { renderDefault, renderImage, renderLink, renderHTML } from './utils';
+import { HumanDatetime } from '@/components/cell-styles/HumanDatetime';
+import { BatteryPercents } from '@/components/cell-styles/BatteryPercents';
+import { StatusExpression } from '@/components/cell-styles/StatusExpression';
+import '@/components/cell-styles/HelloWord.ts';
 import template from './template.html';
 
 const renderFunctions = {
@@ -9,7 +13,7 @@ const renderFunctions = {
 };
 
 export default function init(ngModule) {
-  ngModule.directive('dynamicTableDefaultCell', ($sanitize, $sce) => ({
+  ngModule.directive('dynamicTableDefaultCell', ($sce, $compile) => ({
     template,
     restrict: 'E',
     replace: true,
@@ -17,29 +21,20 @@ export default function init(ngModule) {
       column: '=',
       row: '=',
     },
-    link: ($scope) => {
+    link: ($scope, $element) => {
       // `dynamicTable` will recreate all table cells if some columns changed.
       // This means two things:
       // 1. `column` object will be always "fresh" - no need to watch it.
       // 2. we will always have a column object already available in `link` function.
-      // Note that `row` may change during this directive's lifetime.
-      if ($scope.column.displayAs === 'string') {
-        $scope.allowHTML = $scope.column.allowHTML;
-      } else {
-        $scope.allowHTML = includes(['image', 'link', 'html'], $scope.column.displayAs);
-      }
-
-      const sanitize = $scope.allowHTML ? $sce.trustAsHtml : identity;
-
+      // Note that `row` may change during this directive's lifetime.      
       const renderValue = renderFunctions[$scope.column.displayAs] || renderDefault;
+      const temp = `<div>${renderValue($scope.column, $scope.row)}</div>`;
 
-      $scope.value = sanitize(renderValue($scope.column, $scope.row));
-
-      $scope.$watch('row', (newValue, oldValue) => {
-        if (newValue !== oldValue) {
-          $scope.value = sanitize(renderValue($scope.column, $scope.row));
-        }
-      });
+      if (($scope.column.displayAs === 'string' && $scope.column.allowHTML) || includes(['image', 'link', 'html'], $scope.column.displayAs)) {
+        $element.append($compile(temp)($scope));
+      } else {
+        $element.append(`${temp}`);
+      }
     },
   }));
 }
